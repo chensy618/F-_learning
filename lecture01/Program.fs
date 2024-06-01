8c5ae6c0dab8705011c8f349be7217ee1780b6f6
chensy618 <146075999+chensy618@users.noreply.github.com>
2024-06-01 23:14:37 +0200

    update

--- lecture01/Program.fs
+++ lecture01/Program.fs
@@ -13,3 +13,9 @@ let age = 42
 let PI = 3.14159
 let sum: float = 1.0 + 2.0
 printfn "sum = %f" sum
+let myname = "Luis"
+let company = "Microsoft"
+printfn $"My name is {myname} and I work at {company}"
+let firstnumber = 1
+let secondnumber = 2
+printfn $"The sum of {firstnumber} and {secondnumber} is {firstnumber + secondnumber}"
--- lecture01/obj/Debug/net8.0/lecture01.AssemblyInfo.fs
+++ lecture01/obj/Debug/net8.0/lecture01.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("lecture01")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a21d4dcf846330c637001dfe0eebf7ba662b3985")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d8c5af36c98cacf29237f74c4b7819137c72c3d")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("lecture01")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("lecture01")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
