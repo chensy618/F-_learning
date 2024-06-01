2d8c5af36c98cacf29237f74c4b7819137c72c3d
chensy618 <146075999+chensy618@users.noreply.github.com>
2024-06-01 13:45:58 +0200

    basic grammar

---
+++ lecture01/Program.fs
@@ -0,0 +1,15 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
+let b = true
+printfn "b = %b" b
+let b2 = not b
+printfn "b2 = %b" b2
+let name = "Chris"
+let mutable name2 = "Chris"
+name2 <- "Bob"
+printfn "name = %s" name
+printfn "name2 = %s" name2
+let age = 42
+let PI = 3.14159
+let sum: float = 1.0 + 2.0
+printfn "sum = %f" sum
