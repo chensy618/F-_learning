6f8cb13aecb761cb82e62fa1ed513335233cc337
chensy618 <146075999+chensy618@users.noreply.github.com>
2024-06-02 13:03:24 +0200

    Loop

---
+++ Cards/Program.fs
@@ -0,0 +1,12 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
+let cardno = 12
+
+let carddescription =
+    if cardno = 1 || cardno = 14 then "Ace"
+    elif cardno = 11 then "Jack"
+    elif cardno = 12 then "Queen"
+    elif cardno = 13 then "King"
+    else "Number card"
+
+printfn "The card is: %s" carddescription
---
+++ Loops/Program.fs
@@ -0,0 +1,30 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
+let list = [ 1; 2; 3; 4; 5 ]
+// loop in the list
+for i in list do
+    printfn "The number is: %d" i
+
+// loop until a condition is met, from smallest to largest
+for i = 1 to 10 do
+    printfn "The number is: %d" i
+
+// loop until a condition is met, from largest to smallest
+for i = 10 downto 1 do
+    printfn "The number is: %d" i
+
+// while loop
+open System
+let mutable quit = false
+let no = 11
+
+while not quit do
+    printfn "Type a number: "
+    let str = Console.ReadLine()
+    let number = int str
+
+    if number = no then
+        printfn "You guessed the number"
+        quit <- true
+    else
+        printfn "Try again"
