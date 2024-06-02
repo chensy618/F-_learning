2bdd3d18e1fdbebd431ad53b275c7a8020826a24
chensy618 <146075999+chensy618@users.noreply.github.com>
2024-06-02 10:21:05 +0200

    adding Variables and Errors

---
+++ Errors/Program.fs
@@ -0,0 +1,9 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
+
+[<EntryPoint>]
+let main argv =
+    let aNumber = 0
+    printfn "aNumber = %s" aNumber
+    // this is type-check error, string cannot match int
+    0
---
+++ Variables/Program.fs
@@ -0,0 +1,9 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+[<EntryPoint>] // main function
+let main argv =
+    printfn "Hello from F#"
+    printfn "Welcome to the calculator program"
+    // read input from the console and assign to `sum`
+    let sum = 0
+    printfn "sum = %d" sum
+    0
